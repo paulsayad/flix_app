05cc6b616d0db4c11c30a4429365565f2bbe83c6
sayad <realPaulSayad@gmail.com>
2021-02-12 20:02:08 -0500

    Added app icon and styled launch screen. Ability to view and scroll through list of movies through The Movie Database API. Added posters for each movie.

---
+++ flix_app/MoviesViewController.swift
@@ -0,0 +1,90 @@
+//
+//  MoviesViewController.swift
+//  flix_app
+//
+//  Created by Paul Sayad on 2/12/21.
+//
+
+import UIKit
+import AlamofireImage
+
+// Working with TableView
+// Step 1: Add UITableViewDataSource and UITableViewDelegate -> Then Click Fix to add the two tableView functions (moved below)
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Creating an array of dictionaries
+    var movies = [[String:Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Step 3
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                // as! casts the variable to whatever is after
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                // Step 4: reload table view to call the functions to update it with data
+                self.tableView.reloadData()
+                
+                print(dataDictionary)
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+
+           }
+        }
+        task.resume()
+    }
+    
+    // Step 2: Implement these two Table View Functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // dequeue reusable cells reuses cells that are off screen
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        cell.titleLabel.text = title;
+        cell.synopsisLabel.text = synopsis
+
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+        cell.posterView.af_setImage(withURL: posterUrl)
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
