d7174ff6b8547512b43eb503245c882d67835907
sayad <realPaulSayad@gmail.com>
2021-02-19 20:03:45 -0500

    - Added more detail sections for movies - Added Superhero themed movie poster collection view

---
+++ flix_app/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix_app
+//
+//  Created by Paul Sayad on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
